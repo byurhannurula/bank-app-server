35e709ecdf6d0d10f4422ae41c56bf13602e17ac
Byurhan Beyzat <imbyurhan@gmail.com>
2020-02-16 11:56:20 +0200

    FE: Update header styles

--- web/src/components/header/index.jsx
+++ web/src/components/header/index.jsx
@@ -14,22 +14,39 @@ const SiteLink = ({ label, link, icon }) => (
   </Link>
 )
 
+const currentUser = false
+
 const Header = () => (
   <header className="header">
     <div className="container">
       <span className="logo">TNT-Bank</span>
 
-      <nav className="nav">
-        <ul className="nav__list">
-          {navLinks.map(({ id, label, link, icon }) => (
-            <li className="nav__item" key={id}>
-              <SiteLink label={label} link={link} icon={icon} />
-            </li>
-          ))}
-        </ul>
-      </nav>
+      {currentUser ? (
+        <>
+          <nav className="nav">
+            <ul className="nav__list">
+              {navLinks.map(({ id, label, link, icon }) => (
+                <li className="nav__item" key={id}>
+                  <SiteLink label={label} link={link} icon={icon} />
+                </li>
+              ))}
+            </ul>
+          </nav>
 
-      <SiteLink label="Logout" link="/" />
+          <Link href="/">
+            <a className="nav__link">Logout</a>
+          </Link>
+        </>
+      ) : (
+        <div style={{ display: 'flex' }}>
+          <Link href="/login">
+            <a className="nav__link">Login</a>
+          </Link>
+          <Link href="/register">
+            <a className="nav__link">Register</a>
+          </Link>
+        </div>
+      )}
     </div>
   </header>
 )
