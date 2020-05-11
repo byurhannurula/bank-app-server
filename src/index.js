c0f856ecda37d891a4093da08d55ff300f67a1d7
Byurhan Beyzat <imbyurhan@gmail.com>
2020-05-11 22:19:10 +0300

    Update to fix heroku errors

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ const { app } = require('./config/rest')
 const routes = require('./routes')
 
 const restPort = process.env.REST_PORT || 1000
-const graphqlPort = process.env.GRAPHQL_PORT || 2000
+const graphqlPort = process.env.PORT || 2000
 
 const initServer = async () => {
   console.clear()
 
   await dbConnection()
 
-  // app.use('*', routes)
+  app.use('*', routes)
 
-  // app.listen(restPort, () => {
-  //   console.log(`— Rest API: http://localhost:${restPort}`)
-  // })
+  app.listen(restPort, () => {
+    console.log(`— Rest API: http://localhost:${restPort}`)
+  })
   httpServer.listen(graphqlPort, () => {
     console.log(`— Graphql API: http://localhost:${graphqlPort}/graphql`)
   })
